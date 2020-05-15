81b0f951a83df7ba6d07a462d57844b7387add71
Railane Passos <railanepassos@gmail.com>
2020-05-15 00:39:30 -0300

    Startingn with JavaScript.

---
+++ js4t/main.js
@@ -0,0 +1,5 @@
+
+
+var welcome = "Hello JavaScript - Automation Full Stack from QA Ninja !"
+document.getElementById("welcome").append(welcome)
+console.log("Hello JavaScript - Automation Full Stack from QA Ninja !")
