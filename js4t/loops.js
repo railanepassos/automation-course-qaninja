0513f087322e1918e4995c47a5ea5a01f2d532d0
Railane Passos <railanepassos@gmail.com>
2020-05-22 01:07:41 -0300

    Meeting Loops.

---
+++ js4t/loops.js
@@ -0,0 +1,36 @@
+
+// Loops (Repetition Loops)
+
+    // For is moved by declaration
+    for (var index = 1; index <= 10; index++) {
+        switch (index) {
+            case 1:
+                console.log(`Repeating because one is less than ten !`)
+                break;
+            case 2:
+                console.log(`Repeating because two is less than ten !`)
+                break;
+            case 3:
+            console.log(`Repeating because three is less than ten !`)
+                break;
+            default:
+                console.log(`Repeating because ${index} is less than ten !`)
+                break;
+        }
+    }
+
+    // While is moved for condition and stay this condition while true
+    console.log("---------------------------- Whille ----------------------------")
+    var index = 1
+    while (index <= 10) {
+        console.log(`Repeating because ${index} is less than ten !`)
+        index ++    
+    }
+
+    // For it - job witch arrays
+
+    var avengers = ['Ironman', 'Spiderman', 'Thor', 'Captian America', 'Black Panther', 'Black Window']
+    avengers.forEach(
+        function(value, key){
+            console.log(`${value} in position ${key}`)
+    })
--- js4t/main.js
+++ js4t/main.js
@@ -3,3 +3,11 @@
 var welcome = "Hello JavaScript - Automation Full Stack from QA Ninja !"
 document.getElementById("welcome").append(welcome)
 console.log("Hello JavaScript - Automation Full Stack from QA Ninja !")
+
+
+
+
+
+
+
+
